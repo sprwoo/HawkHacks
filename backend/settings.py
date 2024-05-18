9653318773e50c2921e8fde441587e1fd385a400
Tian <xtyao2015@gmail.com>
2024-05-18 00:43:04 -0400

    added react frontend

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "rest_framework",
     "backend",
     "django.contrib.admin",
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +55,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "backend.urls"
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
 TEMPLATES = [
     {
