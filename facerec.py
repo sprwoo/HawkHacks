d321617879596e21ee448661b1c1bfe2e9f4924b
Hetul Patel <patehetu239@gmail.com>
2024-05-18 12:50:52 -0400

    added frontend

--- facerec.py
+++ facerec.py
@@ -1,7 +1,7 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
-file_name = "imgs/IMG_0276.jpg"
+file_name = "imgs/stockimg.jpg"
 
 img = cv2.imread(file_name)
 #img = cv2.resize(img, (0, 0), fx=0.7, fy=0.7)
@@ -10,7 +10,7 @@ cv2.imshow('img', img)
 k = cv2.waitKey(0)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3)
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
 i = 0
 for (x, y, w, h) in faces:
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from deepface import DeepFace
 
-mainpath = "imgs/selfie.jpg"
+mainpath = "imgs/img1.jpg"
 for x in range(0,3):
     path2 = "stored-faces/" + str(x) + '.jpg'
     result = DeepFace.verify(img1_path = mainpath, img2_path = path2, enforce_detection=False)
