10a13bd76cec2ef222ceb1cd093a936710fe292c
Daniel Yang <veryshortshorty@gmail.com>
2024-05-18 04:36:30 -0400

    holy it works

--- facerec.py
+++ facerec.py
@@ -1,25 +1,7 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
-
-'''cap = cv2.VideoCapture(0)
-while True:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-        roi_g = gray[y:y+h, x:x+h]
-        roi = img[y:y+h, x:x+h]
-
-    cv2.imshow('img',img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-cap.release()
-cv2.destroyAllWindows()'''
-        
-file_name = "imgs/stockimg.jpg"
+file_name = "imgs/IMG_0276.jpg"
 
 img = cv2.imread(file_name)
 #img = cv2.resize(img, (0, 0), fx=0.7, fy=0.7)
@@ -28,15 +10,12 @@ cv2.imshow('img', img)
 k = cv2.waitKey(0)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=2)
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3)
 
 i = 0
 for (x, y, w, h) in faces:
     #cv2.rectangle(gray, (x,y), (x+w, y+h), (255,0,0), 2)
-
     cropped_image = gray[y : y + h, x : x + w]
-
     target_file_name = 'stored-faces/' + str(i) + '.jpg'
     cv2.imwrite(target_file_name, cropped_image)
     i += 1
---
+++ test.py
@@ -0,0 +1,7 @@
+from deepface import DeepFace
+
+mainpath = "imgs/selfie.jpg"
+for x in range(0,3):
+    path2 = "stored-faces/" + str(x) + '.jpg'
+    result = DeepFace.verify(img1_path = mainpath, img2_path = path2, enforce_detection=False)
+    print(result)
