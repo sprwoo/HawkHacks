8405127b039b110c9e0cb4a9b7b3a1f5bed1cfca
Tian <xtyao2015@gmail.com>
2024-05-17 23:59:07 -0400

    added django rest framework

---
+++ backend/views.py
@@ -0,0 +1,37 @@
+from django.http import JsonResponse
+from .models import Picture
+from .serializers import PictureSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET', 'POST'])
+def picture_list(request):
+    if request.method == 'GET':
+        pictures = Picture.objects.all()
+        serializer = PictureSerializer(pictures, many=True)
+        return Response(serializer.data) 
+    if request.method == 'POST':
+        serializer = PictureSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+@api_view(["GET", "PUT", "DELETE"])
+def picture_detail(request, id):
+    try:
+        picture = Picture.objects.get(pk=id)
+    except Picture.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serializer = PictureSerializer(picture)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PictureSerializer(picture, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        picture.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
