bd9cf074918895f657cbc40fe075eca17c11906f
Tian <xtyao2015@gmail.com>
2024-05-19 02:56:07 -0400

    yessssssir

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Picture
+from rest_framework.parsers import FileUploadParser
+
 
 # Converts Picture model instances to JSON
 class PictureSerializer(serializers.ModelSerializer):
@@ -7,3 +9,4 @@ class PictureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Picture
         fields = ['id', 'name', 'email', 'group_image']
+        parser_class = (FileUploadParser,)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import Register from "./Register";
-import Selfie from "./Selfie";
 
 function App() {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -10,18 +9,12 @@ function App() {
   const [showSelfie, setShowSelfie] = useState(false);
   const [response, setResponse] = useState("");
 
-  // form stuff to check if azure works
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [image, setImage] = useState(null);
 
-  const handleImageUpload = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      setSelectedImage(URL.createObjectURL(event.target.files[0]));
-    }
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
       const response = await axios.post(
         "http://127.0.0.1:8000/pictures/",
@@ -38,17 +31,22 @@ function App() {
     }
   };
 
-  const handleStuff = async () => {
+  const handleStuff = async (e) => {
+    e.preventDefault();
     const formData = new FormData();
-    formData.append("image", selectedImage);
-    formData.append("name", nameRef.current.value);
-    formData.append("email", emailRef.current.value);
-    formData.append("phone", phoneRef.current.value);
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("group_image", image);
 
-    const response = await fetch("http://localhost:8000/register_img", {
-      method: "POST",
-      body: formData,
-    });
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/pictures/",
+        formData
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("There was an error submitting the form!", error);
+    }
 
     if (!response.ok) {
       console.error("Failed to send data");
@@ -88,11 +86,15 @@ function App() {
         <button onClick={() => handleEmail()} className="button">
           Send email
         </button>
-        <img width={50} src="http://localhost:8000/group_images/IMG_7755.jpeg" />
+        <img
+          width={50}
+          src="http://localhost:8000/group_images/IMG_7755.jpeg"
+        />
         <button onClick={handleStuff} className="button">
-          Clcik
+          Click
         </button>
-        <form onSubmit={handleSubmit}>
+
+        <form onSubmit={handleStuff}>
           <div className="input-container">
             <input
               type="text"
@@ -106,15 +108,33 @@ function App() {
               onChange={(e) => setEmail(e.target.value)}
               className="input-postgres"
             />
+            <div className="upload-container">
+              <input
+                type="file"
+                accept="image/*"
+                onChange={(e) => setImage(e.target.files[0])}
+                id="selfie-upload"
+                className="file-upload"
+              />
+              <label htmlFor="selfie-upload" className="upload-label">
+                {image ? (
+                  <img
+                    src={image}
+                    alt="Selected"
+                    className="uploaded-image"
+                  />
+                ) : (
+                  "Upload your image"
+                )}
+              </label>
+            </div>
             <button type="submit" className="button submit-button">
-              send to azure
+              Submit
             </button>
           </div>
-          
         </form>
       </div>
       {showRegister && <Register />}
-      {showSelfie && <Selfie />}
       {response && <p>{response}</p>}
     </div>
   );
